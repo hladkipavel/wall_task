f8f4834b5dc64131ab29de19b041fffd69fb5f34
hladkipavel <pgladky22@gmail.com>
2023-06-10 18:11:23 +0200

    some changes with packeges

---
+++ src/main/java/com/pavelhladki/wall/entity/Wall.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Wall implements Structure {
+    private List<Block> blocks;
+
+    public Wall(List<Block> blocks) {
+        this.blocks = blocks;
+    }
+
+    @Override
+    public Optional<Block> findBlockByColor(String color) {
+        for(Block block : blocks) {
+            if(block.getColor().equals(color)){
+                return Optional.of(block);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Block> findBlockByMaterial(String material) {
+        List<Block> matchingBlocks = new ArrayList<>();
+        for(Block block : blocks) {
+            if(block.getMaterial().equals(material)){
+                matchingBlocks.add(block);
+            }
+        }
+        return matchingBlocks;
+    }
+
+    @Override
+    public int count() {
+        return blocks.size();
+    }
+}
